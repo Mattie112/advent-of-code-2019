b9938a6f8cd8ea0227e91d01a9cc5fdfb628520f
Mattie112 <matthijsmvs@gmail.com>
2019-12-14 13:15:06 +0100

    WIP Day 10 part 2

--- day10/day10.php
+++ day10/day10.php
@@ -13,6 +13,45 @@ if ($file = fopen(__DIR__ . "/day10-input.txt", "rb")) {
     }
 }
 
+$map = [];
+$input = ".#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##";
+
+//$input = ".#..#
+//.....
+//#####
+//....#
+//...##";
+
+//$input = ".#....#####...#..
+//##...##.#####..##
+//##...#...#.#####.
+//..#.....X...###..
+//..#.#.....#....##";
+
+$input = explode("\n", $input);
+foreach ($input as $line) {
+    $map[] = str_split($line);
+}
+$astroid_map = [];
 $most_astroids = 0;
 $best_loc_x = 0;
 $best_loc_y = 0;
@@ -38,4 +77,67 @@ foreach ($map as $row_id => $row) {
         }
     }
 }
-echo "Part 1: " . $best_loc_x . "," . $best_loc_y . " (" . $most_astroids . " astroids)";
+echo "Part 1: " . $best_loc_x . "," . $best_loc_y . " (" . $most_astroids . " astroids)" . PHP_EOL;
+
+// Now that we have our best position go and calculate the astroid line of sight for this position (same as the function above but simply run again for a single position)
+$astroid_list = getVisibleAstroidsForPosition($map, $best_loc_y, $best_loc_x);
+
+ksort($astroid_list);
+
+// Now we can loop (clockwise, degrees INCREASING) through our astroids and simple kill the closest we can find
+$kill_count = 0;
+while (true) {
+    foreach ($astroid_list as $degrees => $astroids) {
+        if (empty($astroids)) {
+            continue;
+        }
+        $key = min(array_keys($astroids));
+        $closest = $astroids[$key];
+        unset($astroid_list[$degrees][$key]);
+        $kill_count++;
+        echo $kill_count . " - " . implode(",", $closest) . PHP_EOL;
+        if ($kill_count === 200) {
+            echo "Part 2: " . ($closest[1] + $closest[0]) . PHP_EOL;
+            var_dump($closest);
+            break 2;
+        }
+    }
+    // Everything we can see is killed, check for more astroids
+//    $astroid_list = getVisibleAstroidsForPosition($map, $best_loc_y, $best_loc_x);
+}
+
+
+$a = 1;
+
+function getVisibleAstroidsForPosition($map, $best_loc_y, $best_loc_x)
+{
+    $astroid_list = [];
+    foreach ($map as $row_id => $row) {
+        foreach ($row as $cell_id => $cell) {
+            if ($cell === "." || ($row_id === $best_loc_y && $cell_id === $best_loc_x)) {
+                continue;
+            }
+            $degrees = getDegrees($best_loc_y - $row_id, $best_loc_x - $cell_id);
+            $astroid_list[$degrees][abs($best_loc_y - $row_id) + abs($best_loc_x - $cell_id)] = [$cell_id, $row_id];
+        }
+    }
+    return $astroid_list;
+}
+
+function getDegrees($dX, $dY)
+{
+    if ($dX === 0 && $dY === 0) {
+        return 0;
+    }
+//    $degrees = ($dX < 0) ? rad2deg(atan2($dX, $dY)) + 360 : rad2deg(atan2($dX, $dY));
+    $degrees = rad2deg(atan2($dX, $dY));
+    // Our atan does not think 0 = NORTH so convert it so that 0 = NORTH = UP
+    $degrees = ($degrees - 90) % 360;
+    // All negative values will get +360 degrees so that our clockwise layer can just go through the array from low to high
+    if ($degrees < 0) {
+        $degrees += 360;
+    }
+    return $degrees;
+}
+
+
